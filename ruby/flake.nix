60335293ca099240c3f7c7b61f68bf16e7863c1b
Bob van der Linden <bobvanderlinden@gmail.com>
2021-03-08 13:33:04 +0100

    ruby: add support for bundix

--- ruby/flake.nix
+++ ruby/flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
     nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
-
     nixpkgs-ruby.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -10,22 +9,26 @@
   outputs = { self, nixpkgs, nixpkgs-ruby, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        mkRuby = nixpkgs-ruby.lib.mkRuby;
         pkgs = nixpkgs.legacyPackages.${system};
         rubyVersion = nixpkgs.lib.fileContents ./.ruby-version;
-        ruby = mkRuby { inherit pkgs rubyVersion; };
-        bundler = pkgs.bundler.override { inherit ruby; };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            ruby.devEnv
-            bundler
-            pkgconfig
-            libxml2
-            libxslt
-            libmysqlclient
-            mysql.client
-          ];
+        ruby = nixpkgs-ruby.lib.mkRuby { inherit pkgs rubyVersion; };
+
+        gems = pkgs.bundlerEnv {
+          name = "gemset";
+          inherit ruby;
+          gemfile = ./Gemfile;
+          lockfile = ./Gemfile.lock;
+          gemset = ./gemset.nix;
+          groups = [ "default" "production" "development" "test" ];
         };
+      in {
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              # gems
+              ruby
+              bundix
+            ];
+          };
       });
 }
